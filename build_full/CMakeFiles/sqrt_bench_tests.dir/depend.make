d19443eba0c57c97f05a9c74db931ef8847e57ee
Omkar Kakade <omkar.databrain@gmail.com>
2026-05-25 21:44:45 +0530

    removed build

--- build_full/CMakeFiles/sqrt_bench_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sqrt_bench_tests.
-# This may be replaced when dependencies are built.
--- build_full/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/blaze/Blaze/Projects/sqrt_bench
-# Build directory: /home/blaze/Blaze/Projects/sqrt_bench/build_full
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/blaze/Blaze/Projects/sqrt_bench/build_full/sqrt_bench_tests-b12d07c_include.cmake")
-subdirs("_deps/catch2-build")
-subdirs("_deps/benchmark-build")
